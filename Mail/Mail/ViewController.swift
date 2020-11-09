c026b737e3dc58a1ce7c4bba61b5778f6e9c63ad
Eddy Rogier <eddyrogier@gmail.com>
2020-11-09 22:10:11 +0100

    project added

---
+++ Mail/Mail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mail
+//
+//  Created by Eddy R on 09/11/2020.
+//  Copyright © 2020 Eddy R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
