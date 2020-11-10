0da2be4c8c6fceedb9141a4e3a678c17ebba5a6c
Eddy Rogier <eddyrogier@gmail.com>
2020-11-10 16:39:48 +0100

    scrolling tableview

--- Mail/Mail/ViewController.swift
+++ Mail/Mail/ViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2020 Eddy R. All rights reserved.
 //
 
+// ❔ Quoi   - 🗺 Ou   - ⏳Quand - ✋Comment
+// 🤸🏽 Action - 🗺 Lieu - ⏳Temps - ✋Maniere
+
 import UIKit
 
 struct Mail {
@@ -20,24 +23,90 @@ struct Mail {
 
 class ViewController: UIViewController {
 
+    // MARK: - iVAR - CONSTANT - IB
+    // TABLEVIEW FROM SB
     @IBOutlet weak var tableView: UITableView!
+    // SPINNER RELOAD DATA
     var refreshControl = UIRefreshControl()
+    // HEIGHT ROW : HEADER | FOOTER
     var defaultFirstHeaderRow: CGFloat = 60
     var defaultRestOfHeaderRow: CGFloat = 30
-
+    // DATA HYDRATE TABLEVIEW
     var mailArray = [Mail]()
+    @IBOutlet weak var navigationItemcustom: UINavigationItem!
+    // SCROLLING TABLEVIEW
+    var lastVerticalOffset: CGFloat = 0
+    var headerViewOriginalHeight: CGFloat = 0
     
+    
+    // MARK: - CYCLE LIFE
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        overrideUserInterfaceStyle = .light // force dark theme on ios
         spinnerLoadingTableView() // set data
-        setArrayMail() // set data
+        
+        //setup
+        setArrayMail()
+        setUpNavigationItem()
+        setUpView()
     }
     
+    // MARK: - SETTING
+    fileprivate func setUpNavigationItem() {
+        // set data
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.backgroundColor = UIColor.black
+        
+        self.navigationItem.standardAppearance = navBarAppearance
+        self.navigationItem.compactAppearance = navBarAppearance
+        self.navigationItem.scrollEdgeAppearance = navBarAppearance
+    }
+    fileprivate func setUpView() {
+        self.view.backgroundColor = UIColor.black
+        self.tableView.backgroundColor = UIColor.black // view and tableView on top
+    }
     
 }
 
-extension ViewController: UITableViewDataSource {
+// MARK: - SCROLLING TABLEVIEW
+extension ViewController {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // 📢 : defaultFirstHeaderRow 60 px.
+        let verticalOffset = scrollView.contentOffset.y
+        let scrollAmount = verticalOffset - lastVerticalOffset // get amount scrolling since last time
+        lastVerticalOffset = verticalOffset // set the last offset each time scrolling
+        
+        let alphaVariation = scrollAmount/(defaultFirstHeaderRow)
+        
+        print("vOffset : \(verticalOffset) | scrollAmount : \(scrollAmount) | lastVerticalOffset : \(lastVerticalOffset) | alpahVariation : \(alphaVariation) ")
+        
+        
+        //        switch newConstant {
+        //            case _ where newConstant >= 0:
+        //                newConstant = 1
+        ////                lab.alpha = 0
+        //
+        //            case _ where newConstant <= -defaultFirstHeaderRow/2:
+        //                // -62 -61 -60  <- [0] -> 1 2 3
+        //                newConstant = 0
+        ////                lab.alpha += alphaVariation
+        //
+        //            case _ where newConstant > -defaultFirstHeaderRow/2:
+        //                newConstant = defaultFirstHeaderRow
+        ////                lab.alpha = 0
+        //            default:
+        //                break
+        //        }
+    }
+}
+
+// MARK: - DATASOURCE - TABLEVIEWDELEGATE
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return mailArray.count
     }
@@ -51,16 +120,19 @@ extension ViewController: UITableViewDataSource {
     }
 }
 
+// MARK: - SPINNER RELOAD DATA
 extension ViewController {
     // Spinner reload
     func spinnerLoadingTableView() {
         refreshControl.addTarget(self, action: #selector(self.pullToRefresh), for: .valueChanged)
+        refreshControl.tintColor = UIColor.white
         tableView?.alwaysBounceVertical = true
         tableView?.addSubview(refreshControl)
         
         // custom on refresh mail
         let myString = NSMutableAttributedString(string: "Fetching Data")
-        myString.addAttribute(.foregroundColor, value: UIColor.red, range: NSMakeRange(0, 5))
+        myString.addAttribute(.foregroundColor, value: UIColor.orange, range: NSRange(location: 0, length: myString.length))
+        myString.addAttribute(.foregroundColor, value: UIColor.white, range: NSMakeRange(0, 5))
         refreshControl.attributedTitle = myString
         tableView?.addSubview(refreshControl)
     }
@@ -75,7 +147,7 @@ extension ViewController {
     }
 }
 
-// header // footer
+// MARK: - HEIGHT ROW : HEADER | FOOTER
 extension ViewController {
     
     // header
@@ -88,7 +160,7 @@ extension ViewController {
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let v = UIView()
-        v.backgroundColor = .black
+        v.backgroundColor = .clear
         
         if section != 0 {
             // Label BLACK
@@ -108,11 +180,14 @@ extension ViewController {
             label.frame = CGRect(x: 20, y:0, width: tableView.frame.size.width, height: defaultFirstHeaderRow)
             label.text = mailArray[section].title
             label.textColor = .gray
-            //            label.backgroundColor = .yellow
-            label.backgroundColor = .black
+            
+            //MARK: -
+            // FIXME: a remettre en clear
+//            label.backgroundColor = .clear
+            label.backgroundColor = .orange
+            // MARK: -
             label.font = UIFont.boldSystemFont(ofSize: 40)
             label.textColor = UIColor.white
-            //            label.numberOfLines = 2
             label.minimumScaleFactor = 0.2
             label.adjustsFontSizeToFitWidth = true
             
@@ -132,11 +207,13 @@ extension ViewController {
     }
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let v = UIView()
-        v.backgroundColor = .black
+        
+        v.backgroundColor = .red
         return v
     }
 }
 
+// MARK: - DATA HYDRATE TABLEVIEW
 extension ViewController {
     // Set Mail
     func setArrayMail() {
@@ -145,3 +222,7 @@ extension ViewController {
         mailArray.append(Mail(title: "ACTION MAIL", detail:["Reception","Brouillon","envoyé","Corbeille","Archive"]))
     }
 }
+
+
+
+
