64cd14f6a2f8017e0cf9ee594493c98478b3e9c8
Eddy Rogier <eddyrogier@gmail.com>
2020-11-15 15:24:00 +0100

    cell dynamic & static

---
+++ Mail/Mail/Views/CellS.swift
@@ -0,0 +1,24 @@
+//
+//  CellS.swift
+//  Mail
+//
+//  Created by Eddy R on 15/11/2020.
+//  Copyright © 2020 Eddy R. All rights reserved.
+//
+
+import UIKit
+
+class CellS: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mail/Mail/Views/WiFiCell.swift
@@ -0,0 +1,24 @@
+//
+//  WiFiCell.swift
+//  Mail
+//
+//  Created by Eddy R on 15/11/2020.
+//  Copyright © 2020 Eddy R. All rights reserved.
+//
+
+import UIKit
+
+class WiFiCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
